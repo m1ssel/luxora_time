ae37ff57c12f0f84c90d4250041eac97fbe292f3
m1ssel <artemaz665@gmail.com>
2024-10-20 21:11:24 +0200

    bug fixes

--- src/components/product-info/product-info.jsx
+++ src/components/product-info/product-info.jsx
@@ -1,44 +1,46 @@
-import React from 'react'
+import React from "react";
 
-import { useParams } from 'react-router-dom'
+import { useParams } from "react-router-dom";
 
-import db from '../../assets/db.json'
+import db from "../../assets/db.json";
+
+import "./product-info.scss";
 
 const ProductInfo = () => {
-    const { name } = useParams()
+  const { name } = useParams();
 
-    const anyWatch = db.find(item => item.name === name)
+  const anyWatch = db.find((item) => item.name === name);
 
-    const excludedKeys = ["id", "img", "brand", "name", "price", "color", "text"];
+  const excludedKeys = ["id", "img", "brand", "name", "price", "color", "text"];
 
-    const formatKey = (key) => {
-        return key.charAt(0).toUpperCase() + key.slice(1);
-    };
-    return (
-        <div className='product-info-container'>
-            
-            <div className='spec-container'>
-                <h2 className='spec-main-text'>Product Specification</h2>
-                <table style={{ width: '100%', borderCollapse: 'collapse' }}>
-                    <tbody>
-                        {Object.entries(anyWatch)
-                            .filter(([key]) => !excludedKeys.includes(key))
-                            .map(([key, value]) => (
-                                <tr key={key} style={{ borderBottom: '1px solid #ccc' }}>
-                                    <td style={{ padding: '10px', fontWeight: 'bold' }}>{formatKey(key)}:</td>
-                                    <td style={{ padding: '10px' }}>{value}</td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
-            </div>
-            <div className='descr-container'>
-                <h2 className='descr-main-text'>Description</h2>
-                <p dangerouslySetInnerHTML={{ __html: anyWatch.text }} />
-            </div>
-        </div>
-    )
-}
+  const formatKey = (key) => {
+    return key.charAt(0).toUpperCase() + key.slice(1);
+  };
 
-export default ProductInfo
+  return (
+    <div className="product-info-container">
+      <div className="spec-container">
+        <h2 className="spec-main-text">Product Specification</h2>
+        <table className="spec-table">
+          {Object.entries(anyWatch)
+            .filter(([key]) => !excludedKeys.includes(key))
+            .map(([key, value]) => (
+              <tr key={key} className="table-row">
+                <td>{formatKey(key)}:</td>
+                <td>{value}</td>
+              </tr>
+            ))}
+        </table>
+      </div>
+      <div className="descr-container">
+        <h2 className="descr-main-text">Description</h2>
+        <p
+          dangerouslySetInnerHTML={{ __html: anyWatch.text }}
+          className="descr-text"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProductInfo;
--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,60 +1,69 @@
 import { createContext, useState, useEffect } from "react";
-import db from '../assets/db.json';
+import db from "../assets/db.json";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-    const [cartItems, setCartItems] = useState({});
-    const [favoriteItems, setFavoriteItems] = useState([]);
-    const [formattedTotalAmount, setFormattedTotalAmount] = useState('0');
+  const [cartItems, setCartItems] = useState({});
+  const [favoriteItems, setFavoriteItems] = useState([]);
+  const [formattedTotalAmount, setFormattedTotalAmount] = useState("0");
 
-    const addToFavorites = (product) => {
-        setFavoriteItems((prevFavorites) => [...prevFavorites, product]);
-    };
-
-    const removeFromFavorites = (productId) => {
-        setFavoriteItems((prevFavorites) => 
-            prevFavorites.filter((item) => item.id !== productId)
-        );
-    };
-
-    const updateTotalAmount = (amount) => {
-        setFormattedTotalAmount(amount.toLocaleString('en'));
-    };
-
-    const addToCart = (itemId) => {
-        if (!cartItems[itemId]) {
-            setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
-        } else {
-            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
-        }
-    };
-
-    const removeFromCart = (itemId) => {
-        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
-    };
-
-    useEffect(() => {
-        console.log(cartItems);
-    }, [cartItems]);
-
-    const contextValue = {
-        db,
-        cartItems,
-        addToCart,
-        removeFromCart,
-        formattedTotalAmount,
-        updateTotalAmount,
-        favoriteItems,
-        addToFavorites,
-        removeFromFavorites
-    };
-
-    return (
-        <StoreContext.Provider value={contextValue}>
-            {props.children}
-        </StoreContext.Provider>
+  const addToFavorites = (product) => {
+    // Проверяем, существует ли уже товар в избранном
+    const isAlreadyFavorite = favoriteItems.some(
+      (item) => item.id === product.id
     );
-}
+
+    if (!isAlreadyFavorite) {
+      setFavoriteItems((prevFavorites) => [...prevFavorites, product]);
+    } else {
+      console.log("Этот товар уже добавлен в избранное.");
+    }
+  };
+
+  const removeFromFavorites = (productId) => {
+    setFavoriteItems((prevFavorites) =>
+      prevFavorites.filter((item) => item.id !== productId)
+    );
+  };
+
+  const updateTotalAmount = (amount) => {
+    setFormattedTotalAmount(amount.toLocaleString("en"));
+  };
+
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
+
+  const contextValue = {
+    db,
+    cartItems,
+    addToCart,
+    removeFromCart,
+    formattedTotalAmount,
+    updateTotalAmount,
+    favoriteItems,
+    addToFavorites,
+    removeFromFavorites,
+  };
+
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
 
 export default StoreContextProvider;
